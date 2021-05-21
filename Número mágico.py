f32adbfd12c52d9336d1626c3a60fbfb26710e3d
keren-sofia <84553085+keren-sofia@users.noreply.github.com>
2021-05-20 22:25:11 -0500

    Add files via upload

---
+++ Número mágico.py
@@ -0,0 +1,5 @@
+numero_magico = 12345679
+numero_usuario = int (input ("Por favor escriba su número de usuario (entre 1 y 9)"))
+numero_usuario *= 9
+multiplicacion = (numero_usuario * numero_magico)
+print("El número mágico corresponde a:", multiplicacion)
